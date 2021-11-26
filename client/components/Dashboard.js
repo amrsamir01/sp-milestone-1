0e078ea54f9233a727c414f970849fe4c08ab244
kamr6348 <kamr6348@gmail.com>
2021-11-26 18:21:26 +0200

    design
    a

--- client/components/Dashboard.js
+++ client/components/Dashboard.js
@@ -1,5 +1,9 @@
 import React from "react";
+import "./styles.css";
 
-export default function Dashboard() {
-  return <div>DASHBOARD</div>;
+export default function Dashboard() {//return <div>DASHBOARD</div>;
+   return(  <p className="head" > Dashboard </p>
+   
+  );
+
 }
