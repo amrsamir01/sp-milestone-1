c03454d1a1e09efe33c8dcd2e550e85e15a4ca4e
kamr6348 <kamr6348@gmail.com>
2021-12-03 23:40:14 +0200

    c

--- client/pages/Dashboard.js
+++ client/pages/Dashboard.js
@@ -1,14 +1,16 @@
 import React from "react";
-
+import ReactTable from "react-table";
 
 export default function Dashboard() { //return <div>DASHBOARD</div>;
-   
-  return(  
-   <div>
-   <p className="title" > transaction </p>
-   
-   
-   </div>  
-   );
+ 
+  return (
+    <div>
+       <ReactTable
+          
+           defaultPageSize = {2}
+           pageSizeOptions = {[2,4, 6]}
+        />
+    </div>
+);
 
 }
